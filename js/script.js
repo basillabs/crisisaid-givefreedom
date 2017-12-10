e10ff8f13fdf74f1d6fa4768e86444237c094797
Chris Lee <chris@chrsl.net>
2017-12-09 16:33:40 -0800

    Add Google Analytics

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+  // Interaction: Transition background on scroll
   var documentHeight = $(document).height();
   var colorTransitionBegin = Math.ceil(documentHeight / 4);
   var maxDifference = Math.ceil(documentHeight / 3);
@@ -8,6 +9,7 @@ $(function() {
     $('#bg').css('opacity', opacity);
   });
 
+  // Interaction: Open donation panel
   var donationLink = $('#donate-button').attr('href');
   $('#donate-button').on('click touchstart', function(e) {
     e.preventDefault();
@@ -16,10 +18,18 @@ $(function() {
     }
     $('#donation').addClass('donation--visible')
     $('body').addClass('overflow-hidden');
+
+
+    // Analytics: Log donation panel open
+    gtag('event', 'open_donation_panel');
   });
 
+  // Interaction: Close donation panel
   $('#scrim, #close').click(function() {
     $('#donation').removeClass('donation--visible');
     $('body').removeClass('overflow-hidden');
+
+    // Analytics: Log donation panel open
+    gtag('event', 'close_donation_panel');
   });
 });
